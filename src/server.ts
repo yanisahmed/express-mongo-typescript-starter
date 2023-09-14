fc867196b83f934188a80d8279ab9a7cc8180043
Yanis Ahmed <yanisahmed12@gmail.com>
2023-09-14 13:39:54 +0600

    Initial Files

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,16 @@
 import mongoose from "mongoose";
 import app from "./app";
 import config from "./config";
-// main().catch( error => console.log( error) );
 
 async function bootstrap() {
-  //hqvRGOrUn5Muo0sV
   await mongoose.connect(config.database_url as string);
+  console.log("Database Connected Successfully");
   try {
     app.listen(config.port, () => {
       console.log(`Application listening on port ${config.port}`);
     });
   } catch (error) {
-    console.log(error);
+    console.log("Failed to Connect Database", error);
   }
 }
 
